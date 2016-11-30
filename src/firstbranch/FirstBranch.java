79b14f3e758d128c17dbe5627637eb84c46619aa
samitamshephertz <samita.mahajan@shephertz.co.in>
2016-11-30 12:07:49 +0530

    new project added
    Signed-off-by: samitamshephertz <samita.mahajan@shephertz.co.in>

---
+++ src/firstbranch/FirstBranch.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstbranch;
+
+import com.sun.xml.internal.ws.message.saaj.SAAJHeader;
+
+/**
+ *
+ * @author Naresh
+ */
+public class FirstBranch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello");
+       System.out.println("Test");
+       System.out.println("Samita");
+       System.out.println("ttttt");
+    }
+}
